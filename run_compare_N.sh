1c74386e7d9f4b912e78416a2960fd55409483fd
jasmine <jasmine.nguyenduc@gmail.com>
2022-11-23 11:30:47 +0100

    "delete concentration"

--- run_compare_N.sh
+++ run_compare_N.sh
@@ -4,30 +4,39 @@ declare -a states=("active");
 #declare -a folders=("N_10_4" "N_5_10_4" "N_10_5" "N_5_10_5" "N_10_6");
 #declare -a C=(10000 50000 100000 500000 1000000);
 declare -a folders=("N_10_6");
-declare -a C=(1000000);
+declare -a N=(1000000);
 #declare -a confs=("conf1" "conf2" "conf3");
 declare -a confs=("conf2");
 
-for (( i=0; i<${#C[@]}; i++ ));
+icvf=0.7;
+
+for (( i=0; i<${#N[@]}; i++ ));
 do
     for conf in "${confs[@]}"
     do
         for l in "${locs[@]}";
         do
-            c=${C[i]};
+
+            n=${N[i]};
+            if [[ $l == "extra" ]];then
+                n=$(( (1-icvf)*n ));
+            else
+                n=$(( icvf*n ));
+            fi
+          
             f=${folders[i]};
             for s in "${states[@]}"
             do
-                echo "loc: $l, concentration : $c, conf : $conf, state : $s, folder : $f is starting"
+                echo "loc: $l, num_walkers : $n, conf : $conf, state : $s, folder : $f is starting"
                 
-                python3 make_conf_file.py -l "$l" -n "$c" -c "$conf" -s "$s" -f "$f"
+                python3 make_conf_file.py -l "$l" -n "$n" -c "$conf" -s "$s" -f "$f"
                 #path="/home/localadmin/Documents/MCDS_code/MCDS-dFMRI/MCDC_Simulator_public-master/instructions/demos/output/dynamic_cylinders/$s/$l/$f/$conf/conf.txt";
                 cd "/home/localadmin/Documents/MCDS_code/MCDS-dFMRI/MCDC_Simulator_public-master/src" || exit 
                 ./main "/home/localadmin/Documents/MCDS_code/MCDS-dFMRI/MCDC_Simulator_public-master/docs/conf_file_examples/gammaDistributedCylinders.conf"
 
                 cd ..
                 cd ..
-                echo "loc: $l, concentration : $c, conf : $conf, state : $s, folder : $f is done"
+                echo "loc: $l, num_walkers : $n, conf : $conf, state : $s, folder : $f is done"
                 
             done
         done
