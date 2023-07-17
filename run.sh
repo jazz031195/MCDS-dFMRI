79a223f749bd86d2a697d8ed718ffb79d5dd0251
ideriedm <inesderied@hotmail.ch>
2023-07-17 09:12:23 +0200

    run job cluster

--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -l
+# see make_conf_file.py to see the parameters to use
+
+path="/scratch/ideriedm/Simulation/MCDS-dFMRI/MCDC_Simulator_public-master"
 
 ./MCDC_Simulator_public-master/compile.sh
-./MCDC_Simulator_public-master/MC-DC_Simulator "/home/localadmin/Documents/MCDS_code/MCDS-dFMRI/MCDC_Simulator_public-master/docs/conf_file_examples/gammaDistributedAxons.conf"
+./MCDC_Simulator_public-master/MC-DC_Simulator "${path}/docs/conf_file_examples/meshIntraInitialization_neuron.conf"
         
