05fa17a394d2b5e99950d0837bf1086a9c71a6f3
ideriedm <inesderied@hotmail.ch>
2023-08-15 08:33:14 +0200

    run exchange

--- Analysis/R/branch_no_branch.R
+++ Analysis/R/branch_no_branch.R
@@ -5,9 +5,10 @@ branching <- "no_branching"
 read_csv_no_branch = read.csv(paste(path, branching, sep = ""))
 colnames(read_csv_no_branch) = c('X','loc','N','T', 'Sb', 'b')
 read_csv_no_branch$b <- sprintf(read_csv_no_branch$b, fmt = '%#.3f')
-read_csv_no_branch$T <- as.factor(read_csv_no_branch$T)
-read_csv_no_branch$N <- as.factor(read_csv_no_branch$N)
-read_csv_no_branch$b <- as.factor(read_csv_no_branch$b)
+read_csv_no_branch$b <- as.numeric(read_csv_no_branch$b)
+# read_csv_no_branch$T <- as.factor(read_csv_no_branch$T)
+# read_csv_no_branch$N <- as.factor(read_csv_no_branch$N)
+# read_csv_no_branch$b <- as.factor(read_csv_no_branch$b)
 read_csv_no_branch$branch <- "no branch"
 
 branching <- "branching"
@@ -15,37 +16,15 @@ branching <- "branching"
 read_csv_branch = read.csv(paste(path, branching, sep = ""))
 colnames(read_csv_branch) = c('X','loc','N','T', 'Sb', 'b')
 read_csv_branch$b <- sprintf(read_csv_branch$b, fmt = '%#.3f')
-read_csv_branch$T <- as.factor(read_csv_branch$T)
-read_csv_branch$N <- as.factor(read_csv_branch$N)
-read_csv_branch$b <- as.factor(read_csv_branch$b)
+read_csv_branch$b <- as.numeric(read_csv_branch$b)
+# read_csv_branch$T <- as.factor(read_csv_branch$T)
+# read_csv_branch$N <- as.factor(read_csv_branch$N)
+# read_csv_branch$b <- as.factor(read_csv_branch$b)
 read_csv_branch$branch <- "branch"
 
 data_all = rbind(read_csv_no_branch, read_csv_branch)
 
-stats = compare_means(Sb ~ branch,  data = data_all[data_all$N=="10000" & data_all$T=="10000",], 
-                      paired=FALSE, method="wilcox.test", p.adjust.method="fdr")
-df_p_val <- rstatix::wilcox_test(data_all[data_all$N=="10000" & data_all$T=="10000",], Sb ~ branch, p.adjust.method = "fdr") %>% 
-  rstatix::add_xy_position() %>% filter(p.adj.signif != "ns")
-
-p <- ggplot(data_all[data_all$N=="10000" & data_all$T=="10000",], aes(x=b, y=Sb)) + 
-  geom_boxplot() +
-  ggtitle(paste('b = ', b_, ', N  = ', N_, sep = "")) +
-  theme(plot.title = element_text(hjust = 0.5))
-
-if(length(df_p_val$p) > 0)
-{
-  p <- p + add_pvalue(df_p_val, label = "p.adj.signif", label.size=8, inherit.aes = FALSE)
-}
-
-data_all$b <- as.numeric(data_all$b)
-ggplot(data = data_all[data_all$N=="10000" & data_all$T=="10000" & data_all$b > 0,], aes(x = branch, y = Sb, fill = branch)) + 
-  geom_boxplot() +
-  facet_wrap(~b, strip.position = "bottom", scales = "free_x") +
-  theme(panel.spacing = unit(0, "lines"), 
-        strip.background = element_blank(),
-        strip.placement = "outside")
-
-stat.test <- data_all[data_all$N=="10000" & data_all$T=="10000" & data_all$b > 0,] %>%
+stat.test <- data_all[data_all$N=="10000" & data_all$T=="10000" & data_all$b > 0.000,] %>%
   group_by(b) %>%
   wilcox_test(Sb ~ branch, p.adjust.method = "fdr") 
 stat.test 
@@ -67,14 +46,18 @@ ggboxplot(
 
 
 
-  stat.test <- data_all[data_all$N=="10000" & data_all$T=="10000" & data_all$b > 0,] %>%
-  group_by(b) %>%
-  wilcox_test(Sb ~ branch) 
+stat.test <- data_all[data_all$N=="10000" & data_all$T=="10000" & data_all$b > 0,] %>%
+group_by(b) %>%
+wilcox_test(Sb ~ branch) 
 stat.test 
 stat.test <- stat.test %>% add_xy_position(x = "branch", fun = "mean_se")
+stat.test$y.position <- c(0.874, 0.588, 0.51, 0.413, 0.15)
 ggplot(data_all[data_all$N=="10000" & data_all$T=="10000" & data_all$b > 0,], 
        aes_string(x="as.factor(branch)", y = "Sb", fill="branch")) + 
-geom_boxplot() + facet_wrap(~b, scales="free", ncol=5) + stat_pvalue_manual(stat.test, hide.ns = TRUE) 
-
-+
-theme(strip.background = element_blank())
+geom_boxplot() + facet_wrap(~b, scales="free", ncol=5) +
+stat_pvalue_manual(stat.test, hide.ns = TRUE, inherit.aes=FALSE) + 
+theme(strip.text.x = element_text(size=12,
+                                  face="bold"),
+      strip.background = element_blank(),
+      axis.text.x = element_blank(),
+      axis.ticks.x = element_blank())
---
+++ run_exchange.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -l
+
+# path="/scratch/ideriedm/Simulation/MCDS-dFMRI/MCDC_Simulator_public-master"
+path="MCDC_Simulator_public-master"
+
+declare -a N=("10000"); 
+
+declare -a T=("10000"); 
+
+icvf="0.3";
+p="24";
+l="intra";
+
+for n in "${N[@]}";
+do
+    for t in "${T[@]}";
+    do
+        for ((i=1;i<=4;i++)); 
+        do
+            echo "Starting creation of substrate ... "
+            echo " N : $n"
+            echo " T : $t"
+            
+            ./build/MC-DC_Simulator "${path}/docs/conf_file_examples/Neurons_from_file_intra.conf"
+        done
+        
+    done
+done
